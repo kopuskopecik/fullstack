d789f5a4aaf82d55099035f5ab1da3a22867565e
Erdogan <kopuskopecik@gmail.com>
2023-06-01 14:37:14 +0200

    logo has been added

--- djangosite/home/models.py
+++ djangosite/home/models.py
@@ -7,6 +7,9 @@ class Shopping(models.Model):
     item = models.CharField(max_length=30)
     amount = models.CharField(max_length=30, default = 10)
     status = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ["status"]
 
     def __str__(self):
         return self.item
--- djangosite/static/index.js
+++ djangosite/static/index.js
@@ -16,7 +16,7 @@ async function getApi(pUrl) {
   render(data);
 }
 
-// post new todo
+// post new item
 
 async function postApi(event) {
   const postTodo = getInputData();
@@ -35,7 +35,7 @@ async function postApi(event) {
   getApi(api_url);
 }
 
-// create a object for new todo
+// create a object for new item
 const getInputData = () => {
   let inputData = document.querySelector("input").value;
   const inputDataAmount = document.querySelector(".amount").value;
@@ -52,7 +52,7 @@ const getInputData = () => {
   return data;
 };
 
-//update todo situation
+//update item situation
 async function updateTodo(pId, pTitle, pCompleted) {
   await fetch(`http://localhost:8000/shoppinglists/${pId}/`, {
     method: "PUT",
@@ -71,7 +71,7 @@ async function updateTodo(pId, pTitle, pCompleted) {
   getApi(api_url);
 }
 
-//delete todo with id
+//delete item with id
 
 async function deleteTodo(pId) {
   await fetch(`http://localhost:8000/shoppinglists/${pId}`, {
@@ -80,7 +80,7 @@ async function deleteTodo(pId) {
   getApi(api_url);
 }
 
-// create table for todo list
+// create table for item list
 function render(pData) {
   const tr = pData
     .map(
@@ -98,27 +98,18 @@ function render(pData) {
           ${data.amount}
           </td>
           
-          <td>         
-          <button type="button" class="btn btn-success" onclick="updateTodo(${
-            data.id
-          }, '${data.item}', ${data.status})">
-              <svg xmlns="http://www.w3.org/2000/svg"
-                    width="32" height="32" fill="currentColor" class="bi bi-check-square-fill"
-                    viewBox="0 0 16 16">
-                    <path
-                        d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z" />
-              </svg>
-          </button>
-          <button type="button" class="btn btn-danger" onclick= " deleteTodo(${
+          <td>   
+          <i type="button" class=" fa-solid ${
+            data.status == true
+              ? "fa-circle-minus text-danger"
+              : "fa-circle-check text-success"
+          }" onclick="updateTodo(${data.id}, '${data.item}', ${data.status})">
+          </i>
+          <i type="button" class="text-danger fa-sharp fa-solid fa-trash ms-3" onclick= " deleteTodo(${
             data.id
           })">
-            <svg xmlns="http://www.w3.org/2000/svg"
-                  width="32" height="32" fill="currentColor" class="bi bi-x-square-fill"
-                  viewBox="0 0 16 16">
-                  <path
-                      d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z" />
-              </svg>
-          </button>
+
+          </i>
         </td>
 
       </tr>`
@@ -142,5 +133,5 @@ function render(pData) {
   document.querySelector("#todo-table").innerHTML = table;
 }
 
-//render all todo
+//render all items
 getApi(api_url);
